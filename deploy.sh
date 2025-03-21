88ac8a4c7e5cc3f5a3166a8cda0cea7d8d829f71
Suriyaa NS <suriyaans.22cse@kongu.edu>
2025-03-21 04:16:43 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@
     docker login -u suriyaans07 -p dckr_pat_ZaNZkvTnRFCXon-DcLWjFYSXvgU
     docker tag test suriyaans07/dev
     docker push suriyaans07/dev
-   # docker-compose up -d
+    docker-compose up -d
     
