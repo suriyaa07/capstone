96da7390c9cf59ec82b7a2e848bf8dbb7ab53ffd
Suriyaa NS <suriyaans.22cse@kongu.edu>
2025-03-21 03:49:30 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u ar8888 -p dckr_pat_HU8f1bsQJp3j2DnCttXuuoppo7w
-    docker tag test ar8888/dev
-    docker push ar8888/dev
-    docker-compose up -d
+    docker login -u suriyaans07 -p dckr_pat_ZaNZkvTnRFCXon-DcLWjFYSXvgU
+    docker tag test suriyaans07/dev
+    docker push suriyaans07/dev
+   # docker-compose up -d
     
